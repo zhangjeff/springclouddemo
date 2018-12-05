033e3b66e0be3806ac92fdbc0892ca2d9f4aef04
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2018-12-05 17:50:50 +0800

    first commit

---
+++ eurekademo/eurekaclient/src/main/java/com/jeff/eurekaclient/HelloController.java
@@ -0,0 +1,25 @@
+package com.jeff.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger("HelloController.class");
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:" + instance.getHost());
+        return "Hello World";
+    }
+}
